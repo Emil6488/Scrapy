cb7ad384db2ee457bd32de95de68724cb60546d7
device999 <sattari4@hotmail.com>
2021-11-08 18:55:37 +0100

    added some buggy code with bot

--- formular/auto/views.py
+++ formular/auto/views.py
@@ -15,15 +15,19 @@ from parameters.models import Parameters
 def startQuery(request,pk):    
     try: 
         parameter = Parameters.objects.get(userId=pk)
-        parameter.active = True
-        parameter.save() 
+        if parameter.active == False:
+            parameter.active = True
+            parameter.save()
+            autos = functions.scrapMain(5,pk)
+            for auto in autos:
+                print(auto)
+            return JsonResponse(autos, status=status.HTTP_200_OK, safe=False)
+        else:
+            return JsonResponse({'message': 'Bot is activated'}, status=status.HTTP_403_FORBIDDEN)
     except Parameters.DoesNotExist: 
         param = Parameters(userId = pk,active = True)
         param.save()
-    autos = functions.scrapMain(5,pk)
-    for auto in autos:
-        print(auto)
-    return JsonResponse(autos, status=status.HTTP_200_OK, safe=False) 
+    
 
 @api_view(['GET'])
 def endQuery(request,pk):
@@ -40,13 +44,12 @@ def scrapLoop(request,pk):
     try: 
         parameter = Parameters.objects.get(userId=pk, active=True)
     except Parameters.DoesNotExist: 
-        return JsonResponse({'message': 'The parameters does not exist'}, status=status.HTTP_404_NOT_FOUND)
+        return JsonResponse({'message': 'The parameters does not exist'}, status=status.HTTP_403_FORBIDDEN)
     if parameter.active == False:
-        return JsonResponse({'message': 'Activate start'}, status=status.HTTP_403_FORBIDDEN)
+        return JsonResponse({'message': 'Activate start'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
     url = "https://losangeles.craigslist.org/d/cars-trucks/search/cta?"
     query = functions.generateQuery(parameter)
-    print(url+query)
-    autos = functions.scrapMain(5, pk,url+query,False)
+    autos = functions.scrapMain(15, pk,url+query,False)
     responseAutos = []
     for auto in autos:
         print(auto["link"])
